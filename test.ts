eaded4f21322032eca46a5470573d3c5d9b47519
youme <mrsimplelife@naver.com>
2021-03-14 19:08:30 +0900

    계산기 함수

--- test.ts
+++ test.ts
@@ -1,22 +1,27 @@
 {
   /**
-   * Type Assertions 💩
+   * Let's make a calculator 🧮
    */
-  function jsStrFunc(): any {
-    return 2;
+  type Commend = "add" | "substract" | "multiply" | "divide" | "remainder";
+  function calculate(commend: Commend, n1: number, n2: number): number {
+    switch (commend) {
+      case "add":
+        return n1 + n2;
+      case "substract":
+        return n1 - n2;
+      case "multiply":
+        return n1 * n2;
+      case "divide":
+        return n1 / n2;
+      case "remainder":
+        return n1 * n2;
+      default:
+        throw new Error("너 틀렸어");
+    }
   }
-  const result = jsStrFunc();
-  console.log((result as string).length);
-  console.log((<string>result).length);
-
-  const wrong: any = 5;
-  console.log((wrong as Array<number>).push(1)); // 😱
-
-  function findNumbers(): number[] | undefined {
-    return undefined;
-  }
-  const numbers = findNumbers()!;
-  numbers.push(2);
-
-  const button = document.querySelector("class")!;
+  console.log(calculate("add", 1, 3)); // 4
+  console.log(calculate("substract", 3, 1)); // 2
+  console.log(calculate("multiply", 4, 2)); // 8
+  console.log(calculate("divide", 4, 2)); // 2
+  console.log(calculate("remainder", 5, 2)); // 1
 }
